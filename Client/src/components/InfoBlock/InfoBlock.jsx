eeddd63e6b664f9055ee7e9ffab94cb310f50411
Leonid Deev <ltupov2003@yandex.ru>
2023-04-19 21:37:38 +0300

    Full Create InfoBlockÐ

--- Client/src/components/InfoBlock/InfoBlock.jsx
+++ Client/src/components/InfoBlock/InfoBlock.jsx
@@ -1,12 +1,16 @@
 import React, {useEffect, useState} from "react";
 import "./InfoBlock.css";
 import Plug from "./../../image/Plug.png";
+import DiscordImage from "./../../image/Discord.svg";
+import SteamImage from "./../../image/steam 1.svg";
+import VkImage from "./../../image/VK.svg";
+import axios from "axios";
 
 export const InfoBlock = () => {
     const [Image, SetImage] = useState(Plug);
-    const [Status, SetStatus] = useState("Loading...");
+    const [Status, SetStatus] = useState("Загрузка...");
     const [Nickname, SetNickname] = useState()
-    const [Tag, SetTag] = useState();
+    const [TagProfile, SetTagProfile] = useState();
     const [FullName, SetFullName] = useState();
     const [Discord, SetDiscord] = useState(<></>);
     const [Steam, SetSteam] = useState(<></>);
@@ -16,8 +20,62 @@ export const InfoBlock = () => {
     const [PhoneNumber, SetPhoneNumber] = useState();
 
     useEffect(() => {
+        async function LoadingInfo()
+        {
+            try {
+                const response = await axios.get('http://localhost:2999/api/get/profile'); // делаем GET-запрос на сервер с параметром responseType, чтобы получить изображение в формате blob
+                const imgUrl = await axios.get(response.data.Image, { responseType: 'blob' });
+                SetImage(await window.URL.createObjectURL(new Blob([imgUrl.data])));
 
-    }, [Status])
+                const jsonData = await response.data;
+                SetStatus(jsonData.Status);
+                SetNickname(jsonData.Nickname);
+                SetTagProfile(jsonData.TagProfile);
+                SetFullName(jsonData.FullName);
+                switch (jsonData.Discord)
+                {
+                    case "":
+                        break;
+                    default:
+                        SetDiscord(
+                            <div>
+                                <img src={DiscordImage}/>
+                                <p>{jsonData.Discord}</p>
+                            </div>)
+                }
+                switch (jsonData.Steam)
+                {
+                    case "":
+                        break;
+                    default:
+                        SetSteam(
+                            <div>
+                                <img src={SteamImage}/>
+                                <p>{jsonData.Steam}</p>
+                            </div>)
+                }
+                switch (jsonData.Vk)
+                {
+                    case "":
+                        break;
+                    default:
+                        SetVk(
+                            <div>
+                                <img src={VkImage}/>
+                                <p>{jsonData.Vk}</p>
+                            </div>)
+                }
+                SetStudyGroup(jsonData.StudyGroup);
+                SetStudentIdNumber(jsonData.StudentIdNumber);
+                SetPhoneNumber(jsonData.PhoneNumber)
+            } catch (error) {
+                LoadingInfo();
+            }
+        }
+
+        LoadingInfo();
+    }, [])
+
 
 
     return(
@@ -25,17 +83,17 @@ export const InfoBlock = () => {
             <div>
                 <img src={Image} alt="Фото аккаунта"/>
                 <div>
-                    <div id="Ellipse" className={(Status === "Loading...") ? "Loading" : Status}></div>
-                    <p>{Status}</p>
+                    <div id="Ellipse" className={(Status === "Загрузка...") ? "Loading" : Status}></div>
+                    <p>{(Status === "Загрузка...") ? Status : ((Status === "Online") ? "в сети" : "не в сети")}</p>
                 </div>
             </div>
             <div>
                 <div>
                     <div id="RowNicknameAndTag">
-                        <p id="Nickname">{(Status === "Loading...") ? Status : Nickname}</p>
-                        <p id="Tag">{(Status === "Loading...") ? "loading..." : Tag}</p>
+                        <p id="Nickname">{(Status === "Загрузка...") ? Status : Nickname}</p>
+                        <p id="Tag">{(Status === "Загрузка...") ? "загрузка..." : TagProfile}</p>
                     </div>
-                    <p id="FullName">{(Status === "Loading...") ? Status : FullName}</p>
+                    <p id="FullName">{(Status === "Загрузка...") ? Status : FullName}</p>
                     <div id="ColumnSocialNetworks">
                         {Discord}
                         {Steam}
@@ -50,13 +108,13 @@ export const InfoBlock = () => {
             <div>
                 <p>Информация о пользователе:</p>
                 <div id="StudyGroup">
-                    <p>Учебная группа: <span>{(Status === "Loading...") ? Status : StudyGroup}</span></p>
+                    <p>Учебная группа: <span>{(Status === "Загрузка...") ? Status : StudyGroup}</span></p>
                 </div>
                 <div id="StudentIdNumber">
-                    <p>Номер студенческого: <span>{(Status === "Loading...") ? Status : StudentIdNumber}</span></p>
+                    <p>Номер студенческого: <span>{(Status === "Загрузка...") ? Status : StudentIdNumber}</span></p>
                 </div>
                 <div id="PhoneNumber">
-                    <p>Номер телефона: <span>{(Status === "Loading...") ? Status : PhoneNumber}</span></p>
+                    <p>Номер телефона: <span>{(Status === "Загрузка...") ? Status : PhoneNumber}</span></p>
                 </div>
             </div>
         </div>
--- Server/Server.js
+++ Server/Server.js
@@ -9,12 +9,27 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(express.static("image"));
+
 app.listen(PORT, () => {
     console.log(`Сервер запущен по адресу: http://localhost:${PORT}/, Клиентская часть ещё не запущена, для её запуска создайте новую консоль(старую не закрывать), перейдите в Client и пропишите npm start`);
 })
 
-app.get('/api/get', (req, res) => {
-    res.status(200).json({})
+app.get('/api/get/profile', (req, res) => {
+    const data = {
+        Image: `http://localhost:${PORT}/Ellipse 188.png`,
+        Status: 'Online',
+        Nickname: "nadllin",
+        TagProfile: "@nadllin",
+        FullName: "Матвиенко Надежда Владимировна",
+        Discord: "nadllin#8537",
+        Steam: "nadllin",
+        Vk: "nadin_matvienko",
+        StudyGroup: "ТКБО-02-21",
+        StudentIdNumber: "21T0345",
+        PhoneNumber: "8-963-683-01-20"
+    };
+    res.json(data);
 })
 
 app.post('/api/registration', (req,res) => {
